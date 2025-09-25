31ee43241f12d28b5162a9855d35afbcc844fd7d
Projetolivros <davi_ode@hotmail.com>
2025-09-25 19:03:35 -0300

    app.py

--- app.py
+++ app.py
@@ -6,7 +6,11 @@ app = Flask(__name__)
 @app.route('/gutendex')
 def gutendex_proxy():
     try:
-        response = requests.get('https://gutendex.com/books', params=request.args)
+        response = requests.get(
+            'https://gutendex.com/books',
+            params=request.args,
+            timeout=10  # tempo máximo de espera em segundos
+        )
         return jsonify(response.json())
     except Exception as e:
         return jsonify({'error': str(e)}), 500
